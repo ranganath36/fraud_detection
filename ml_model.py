16d4a5c3f440527ba0079bb6732fd6cd0f5e3882
ranganath36 <rao.ranganaths@gmail.com>
2022-10-29 13:32:51 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 # adding all ml related code here
+
+def model1():
+    print("Adding code for ML model 1")
+
+
+def model2():
+    print("Adding code for ML model 2")
+
+
+
+def model3():
+    print("Adding code for ML model 3")
